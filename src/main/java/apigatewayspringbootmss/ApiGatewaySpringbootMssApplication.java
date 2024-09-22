8db3aa3047f4a20cc7bfd036f9649a3c2d27604f
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-09-22 15:21:55 +0530

    api gateway init

---
+++ src/main/java/apigatewayspringbootmss/ApiGatewaySpringbootMssApplication.java
@@ -0,0 +1,13 @@
+package apigatewayspringbootmss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewaySpringbootMssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewaySpringbootMssApplication.class, args);
+    }
+
+}
